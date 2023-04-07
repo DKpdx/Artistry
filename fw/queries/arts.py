c154e1780c6fd245be8a366074fa88ac13a606fb
Peter <petertrinh00@gmail.com>
2023-04-06 19:08:49 -0700

    arts update, get_all, get_one changed

--- fw/queries/arts.py
+++ fw/queries/arts.py
@@ -28,28 +28,32 @@ class ArtOutWithAccount(ArtOut):
     username: str
 
 class ArtQueries:
-    def get_one(self, art_id: int) -> Optional[ArtOut]:
+    def get_one(self, art_id: int) -> Optional[ArtOutWithAccount]:
         try:
             with pool.connection() as conn:
                 with conn.cursor() as db:
                     result = db.execute(
                         """
                         SELECT
-                            id,
-                            title,
-                            category,
-                            art_pic_url,
-                            description,
-                            price,
-                            username
+                            arts.id,
+                            arts.user_id,
+                            arts.title,
+                            arts.category,
+                            arts.art_pic_url,
+                            arts.description,
+                            arts.price,
+                            users.username
                         FROM arts
-                        WHERE id = %s
+                        INNER JOIN users
+                        ON users.id=arts.user_id
+                        WHERE arts.id = %s
                         """,
                         [art_id]
                     )
                     record = result.fetchone()
                     if record is None:
                         return None
+                    print(record)
                     return self.record_to_art_out(record)
         except Exception as e:
             return {"message": "Could not get art"}
@@ -69,7 +73,7 @@ class ArtQueries:
         except Exception as e:
             return False
 
-    def update(self, art_id: int, art: ArtIn) -> Union[ArtOut, Error]:
+    def update(self, art_id: int, art: ArtIn, user_id: int) -> Union[ArtOut, Error]:
         try:
             with pool.connection() as conn:
                 with conn.cursor() as db:
@@ -92,19 +96,29 @@ class ArtQueries:
                             art_id
                         ]
                     )
-                    return self.art_in_to_out(art_id, art)
+                    return self.art_in_to_out(art_id, art, user_id)
         except Exception as e:
             return {"message": "Could not update art"}
 
-    def get_all(self) -> Union[List[ArtOut], Error]:
+    def get_all(self) -> Union[List[ArtOutWithAccount], Error]:
         try:
             with pool.connection() as conn:
                 with conn.cursor() as db:
                     result = db.execute(
                         """
-                        SELECT id, title, category, art_pic_url, description, price, username
-                        FROM arts;
-                        """
+                        SELECT
+                            arts.id,
+                            arts.user_id,
+                            arts.title,
+                            arts.category,
+                            arts.art_pic_url,
+                            arts.description,
+                            arts.price,
+                            users.username
+                        FROM arts
+                        INNER JOIN users
+                        ON users.id=arts.user_id;
+                        """,
                     )
                     return [
                         self.record_to_art_out(record)
@@ -148,6 +162,7 @@ class ArtQueries:
         return ArtOut(id=id, **data, user_id=user)
 
     def record_to_art_out(self, record):
+        print(record)
         return ArtOutWithAccount(
             id=record[0],
             user_id=record[1],
--- fw/routers/arts.py
+++ fw/routers/arts.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends, Response
 from typing import List, Union, Optional
-from queries.arts import Error, ArtIn, ArtQueries, ArtOut
+from queries.arts import Error, ArtIn, ArtQueries, ArtOut, ArtOutWithAccount
 from authenticator import authenticator
 
 
@@ -13,7 +13,7 @@ def create_art(
 ):
     return repo.create(art, account_data["id"])
 
-@router.get("/arts", response_model=Union[List[ArtOut], Error])
+@router.get("/arts", response_model=Union[List[ArtOutWithAccount], Error])
 def get_all(
     repo: ArtQueries = Depends(),
 ):
@@ -22,17 +22,32 @@ def get_all(
 @router.put("/arts/{art_id}", response_model=Union[ArtOut, Error])
 def update_art(
     art_id: int,
+    response: Response,
     art: ArtIn,
     repo: ArtQueries = Depends(),
+    account_data: dict = Depends(authenticator.get_current_account_data)
 ) -> Union[ArtOut, Error]:
-    return repo.update(art_id, art)
+    try:
+        result = repo.get_one(art_id)
+        if account_data["id"] == result.user_id:
+            try:
+                return repo.update(art_id, art, result.user_id)
+            except Exception:
+                response.status_code = 400
+                return {"message": "Unable to update art"}
+        else:
+            response.status_code = 401
+            return {"message": "You're not allowed to update the art"}
+    except:
+        response.status_code = 404
+        return {"message": "Art can't be found"}
 
-@router.get("/arts/{art_id}", response_model=Optional[ArtOut])
+@router.get("/arts/{art_id}", response_model=Optional[ArtOutWithAccount])
 def get_one_art(
     art_id: int,
     response: Response,
     repo: ArtQueries = Depends(),
-) -> ArtOut:
+) -> ArtOutWithAccount:
     art = repo.get_one(art_id)
     if art is None:
         response.status_code = 404
