3205ff4a22a5054f4f6c57cc603c379ddb92cb9f
Chase Whitaker <whitaker.chase99@gmail.com>
2023-04-18 19:16:59 +0000

    i made a main page with react, still needs to work on function #issue13

---
+++ ghi/src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Footer = () => {
+    return(
+        <div className='bg-white border-t-2 shadow-sm shadow-gray-300 sticky bottom-0 h-[40px] w-full flex items-center justify-center'>
+            <p className='font-bold text-[18px]'>This is a footer</p>
+        </div>
+    );
+};
+export default Footer;
