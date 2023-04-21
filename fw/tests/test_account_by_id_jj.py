c0f5d0f3afe227adb16a28f3c476a1d300006f38
John Jackson <jonathrjacks@gmail.com>
2023-04-21 13:02:32 -0700

    cleaned up unit test for get account by id

--- fw/tests/test_account_by_id_jj.py
+++ fw/tests/test_account_by_id_jj.py
@@ -5,7 +5,7 @@ from authenticator import authenticator
 
 client = TestClient(app)
 
-class EmptyUserQueries:
+class SingleUserQueries:
 
     def get_account_by_id(self, id: int):
         if id == 1:
@@ -26,19 +26,23 @@ test_account = {
 def account_override():
     return test_account
 
-
-def test_get_account_by_id():
-    app.dependency_overrides[AccountQueries] = EmptyUserQueries
+def app_override_setup():
+    app.dependency_overrides[AccountQueries] = SingleUserQueries
     app.dependency_overrides[
         authenticator.try_get_current_account_data
     ] = account_override
 
+def test_get_account_by_id():
+    app_override_setup()
 
     id = 1
     response = client.get(f"/accounts/{id}")
     assert response.status_code == 200
     assert response.json() == test_account
 
+
+def test_get_account_by_invalid_id():
+    app_override_setup()
 
     id = 99999
     response = client.get(f"/accounts/{id}")
