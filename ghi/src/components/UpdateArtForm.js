4e8fde20d5f26f01cba2da6c046ce6eb452e57f3
Chase <whitaker.chase99@gmail.com>
2023-04-26 14:42:12 -0700

    fixing the update art form build errrors

--- ghi/src/components/UpdateArtForm.js
+++ ghi/src/components/UpdateArtForm.js
@@ -151,6 +151,21 @@ function UpdateArtForm() {
                   className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                   value={description}
                 />
+                <div>
+                  <label htmlFor="price" className="sr-only">
+                    Price
+                  </label>
+                  <input
+                    onChange={handlePriceChange}
+                    placeholder="Price"
+                    required
+                    type="text"
+                    name="price"
+                    id="price"
+                    className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                    value={price}
+                  />
+                </div>
               </div>
             </div>
             <div>
